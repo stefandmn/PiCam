dc2e692938886419902f07f137e23fe8102016b4
SDA <damian.stefan@gmail.com>
2016-05-22 19:35:53 +0300

    Add sample files

---
+++ samples/serverandcam0.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Get path variables
+_APP_SAMPLES=`dirname $0`
+_APP_HOME=$(cd $_APP_SAMPLES/../;pwd)
+
+# Start service and Pi camera
+python $_APP_HOME/picam.py "start server and start service on #0 and enable property streaming on #0"
