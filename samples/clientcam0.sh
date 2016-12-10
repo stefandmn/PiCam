eabd16209d9b4a42f28cba779b8a0ba7653ee05b
SDA <stefandmn@users.noreply.github.com>
2016-12-10 09:02:13 +0200

    Delete clientcam0.sh

--- samples/clientcam0.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get path variables
-_APP_SAMPLES=`dirname $0`
-_APP_HOME=$(cd $_APP_SAMPLES/../;pwd)
-
-# Start service on Pi camera - the command will return you the prompt
-python $_APP_HOME/picam.py "start service on #0 and set property CameraResolution=320,240 on #0 and enable property CameraStreaming on #0"
