a77b97baaccdce64138bd648cb0a8c4e03128df3
SDA <stefandmn@users.noreply.github.com>
2016-12-10 09:03:15 +0200

    Delete serverstart0.sh

--- samples/serverstart0.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get path variables
-_APP_SAMPLES=`dirname $0`
-_APP_HOME=$(cd $_APP_SAMPLES/../;pwd)
-
-# Start server instance - will not return you the prompt in console (CTRL ^C mean end of server execution)
-python $_APP_HOME/picam.py "init server"
