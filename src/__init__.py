baf3be059b6d528f46664f043e7ab4d06fd9e7f2
stefandmn <damian.stefan@gmail.com>
2016-12-30 22:37:41 +0200

    Commit for unidentified release

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+# PiCam Module Definition"
 __project__   = "Clue"
 __module__    = "PiCam"
 __email__     = "damian.stefan@gmail.com"
@@ -7,6 +8,7 @@ __copyright__ = "Copyright (c) 2015-2017, AMSD"
 __license__   = "GPL-2"
 __version__   = "1.0"
 
+# PiCam Public Classes and API
 from picam import Camera
 from picam import PiCamClient
 from picam import PiCamServer
